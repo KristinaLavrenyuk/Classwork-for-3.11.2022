cdd235f00921ffaa396d9b4c71e9f96d6526dd70
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-11-05 15:24:39 +0300

    Add files via upload

---
+++ NumberWithSpace.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, i = 0, num = 0;
+    cin >> a;
+    int array[19]{};
+
+    while (true)
+    {
+        num = a % 10;
+        array[i] = num;
+        a /= 10; 
+        if (a == 0) break;
+        i++;
+    }
+    for (int j = i; j >= 0; j--)  cout << array[j] << " ";
+    system("pause");
+}
